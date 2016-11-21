d1106db60eab531d66217110a68eafa255a6a5ff
James Lennon <jameslennon321@gmail.com>
2016-11-20 21:12:57 -0500

    Began creating Game class

---
+++ src/game.py
@@ -0,0 +1,24 @@
+
+
+class Game(object):
+
+	def __init__(self):
+		pass
+
+	def add_listener(self):
+		pass
+
+	def loop(self):
+		pass
+
+	def run_agent(self, agent):
+		pass
+
+	def apply_action(self, state, action):
+		pass
+
+
+class GameListener(object):
+
+	def handle_loop(self, gameState):
+		pass
