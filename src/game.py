2bdb5a279588f7c80ead573957f53b6db1db15af
James Lennon <jameslennon321@gmail.com>
2016-11-21 13:28:27 -0500

    Began adding team code to game class'

--- src/game.py
+++ src/game.py
@@ -3,12 +3,22 @@
 class Game(object):
 
 	def __init__(self, width, height, state_resolution):
-		self.width = width
-		self.height = height
+		self.width            = width
+		self.height           = height
 		self.state_resolution = state_resolution
 
+		self.agents    = [[],[]]
 		self.listeners = []
 		self.gameState = GameState()
+
+	def add_agent(self, agent, team):
+
+		# check valid team
+		if team not in (0,1):
+			print "ERROR: Invalid team {}; must be 0 or 1.".format(team)
+			return
+
+		self.agents[team].append(agent)
 
 	def add_listener(self, listener):
 		self.listeners.append(listener)
