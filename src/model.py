65f33e8a56cbcebb3d04abaff1db78c2dac4e983
James Lennon <jameslennon321@gmail.com>
2016-11-22 00:54:40 -0500

    Updating transition model

--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 from enum import Enum
-import reward
+import config
+import copy
 
 class Action(Enum):
 	stay       = (0,0)
@@ -14,13 +15,11 @@ class Action(Enum):
 
 class State(object):
 
-	# def __init__(self, dist_team, dist_opps, have_flag, enemy_side, flag_taken, dist_flag, dist_opp_flag, jail):
 	def __init__(self):#, dist_team, dist_opps, have_flag, enemy_side, flag_taken, dist_flag, dist_opp_flag, jail):
-		self.global_positions = [[],[]]
 		self.team = None # either 0 or 1 for which team the State is on
 		self.dist_team = []
 		self.dist_opps = []
-		self.have_flag = False
+		self.has_flag = False
 		self.enemy_side = False
 		self.flag_taken = False
 		self.dist_flag = (0,0) 
@@ -38,14 +37,40 @@ class GameState(object):
 class TransitionModel(object):
 	
 	def __init__(self):
-		pass
+		self.jail_pos = (-1,-1)
 
-	def apply_action(self, state, action):
-		original_pos = state.pos
-		state.pos[0] = state.pos[0] + action.value[0]
-		state.pos[1] = state.pos[1] + action.value[1]
-		if state.pos is a jail state: 
-			state.jail = True 
+	def is_tagged(self, state):
+		
+		# check if you can be tagged
+		if not state.enemy_side and not state.has_flag:
+			return False
+
+		# return if we're touching an enemy
+		return any(map(lambda x: x <= config.PLAYER_RADIUS, state.dist_opps))
+
+	def is_in_jail(self, state):
+		return state.pos == self.jail_pos
+
+	def apply_action(self, old_state, action, game_state):
+
+		state = copy.copy(old_state)
+
+		if state.jail:
+			state.jail = False
+			# TODO: make spawn point
+			state.pos  = (0,0)
+
+		if self.is_tagged(state): 
+			state.jail = True
+			state.pos  = jail_pos
+
+		if not state.jail:
+			state.pos[0] = state.pos[0] + action.value[0]
+			state.pos[1] = state.pos[1] + action.value[1]
+
+		other_team = state.team ^ 1 # use XOR operator to toggle team
+		distances  = map(lambda x: , game_state.states[other_team])
+
 		for dist in state.dist_team:
 			team_pos[0] = original_pos[0]+dist[0]
 			team_pos[1] = original_pos[1]+dist[1]
@@ -71,7 +96,7 @@ class RewardModel(object):
 			return config.JAIL_REWARD
 
 		# calculate reward for getting closer to flag
-		flag_reward = config.FLAG_REWARD_WEIGHT * 
+		flag_reward = config.FLAG_REWARD_WEIGHT * \
 					(new_state.dist_opp_flag - state.dist_opp_flag)
 
 		return flag_reward
