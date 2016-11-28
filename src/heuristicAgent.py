f0c5dc5f90a08dd8f278d453c9d6890687549927
James Lennon <jameslennon321@gmail.com>
2016-11-28 17:08:23 -0500

    added heuristic agent

---
+++ src/heuristicAgent.py
@@ -0,0 +1,28 @@
+from agent import *
+from model import *
+
+
+class HeuristicAgent(Agent):
+
+    def choose_action(self, state, game_state):
+        other_team = state.team ^ 1
+        enemy_pos  = map(lambda s: s.pos, game_state.states[other_team])
+
+        adj = game_state.get_adjacent(state)
+
+        min_dist = lambda p: min(map(lambda o: util.distance(p, o), enemy_pos))
+
+        best_action = None
+        best_score = None
+        for a in adj:
+            score = min_dist(adj[a].pos)
+            if best_score is None or score < best_score:
+                best_action = a
+                best_score = score
+
+        return best_action
+
+
+
+    def observe_transition(self, state, action, reward, new_state):
+        pass
